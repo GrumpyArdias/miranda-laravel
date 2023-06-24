6a3cebe27cab6d4033af54879bb4d848aadf55ea
GrumpyArdias <this.mario@gmail.com>
2023-06-25 00:45:46 +0200

    fixing import issues, fixing orders form, fixing minor issues for navigation, added a order modal

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,7 +20,7 @@
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
-            <a href="/">
+            <a href="index">
                 <div class="container-menu__box-h">
                     <p>H</p>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
