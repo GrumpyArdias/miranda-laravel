25ce8540355a180ffe75eb115f2904b8d6aaf7fc
GrumpyArdias <this.mario@gmail.com>
2023-06-25 14:29:35 +0200

    Added modals to all forms

--- resources/views/room-details.blade.php
+++ resources/views/room-details.blade.php
@@ -30,28 +30,27 @@
             <input type="date" placeholder="30th march 2020" name="checkOut" pattern="\d{1,2}/\d{1,2}/\d{4}"></input>
         </div>
         <button>CHECK AVAILABILITY</button>
-        @if($status=Session::get("success"))
-
-        <div class="pageDetailsAvailability__modalContainer" id="availabilityModal">
-            <div class="pageDetailsAvailability__modal">
-                <h2 class="pageDetailsAvailability__modal__title">
-                    ¡Thank you for your request!
-
-                </h2>
-                <p class="pageDetailsAvailability__modal__text">
-                    We have received it correctly. Someone from our Team will get back to you very soon.
-                    The Miranda Hotel
-                </p>
-                <button class="button button-variant1 pageDetailsAvailability__modal__btn" id="modalBtn">
-                    ACEPT
-                </button>
-            </div>
-        </div>
-
-        @endif
-
-
     </form>
+    @if($status=Session::get("success"))
+
+    <div class="pageDetailsAvailability__modalContainer" id="availabilityModal">
+        <div class="pageDetailsAvailability__modal">
+            <h2 class="pageDetailsAvailability__modal__title">
+                ¡Thank you for your request!
+
+            </h2>
+            <p class="pageDetailsAvailability__modal__text">
+                We have received it correctly. Someone from our Team will get back to you very soon.
+                The Miranda Hotel
+            </p>
+            <button class="button button-variant1 pageDetailsAvailability__modal__btn" id="modalBtn">
+                ACEPT
+            </button>
+        </div>
+    </div>
+
+    @endif
+
 </section>
 <div class="paragraph-lorem">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
